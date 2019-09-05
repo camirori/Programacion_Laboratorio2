8977142be70cf7b8ff6f83f415e7f65300c36ae0
alumno <alumno@utnfra.com.ar>
2019-09-04 22:07:26 -0300

    Clase 05

---
+++ Ejercicios/CampusEj05_Clase05/Repaso/Negocio/Estante.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Estante
+    {
+        #region Propiedades
+        private Producto[] productos;
+        public int ubicacionEstante;
+        #endregion
+
+        #region Constructores
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion): this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        #endregion
+
+        #region Get
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            string retorno = String.Format("Ubicacion del estante: {0}\nProductos:\n", e.ubicacionEstante);
+            for(int i=0; i<e.productos.Length; i++)         //puedo usar foreach
+            {
+                retorno += Producto.MostrarProducto(e.productos[i]);
+            }
+            return retorno;
+        }
+        #endregion
+
+        #region Sobrecarga
+        /// <summary>
+        /// Indica si el producto se encuentra en el estante
+        /// </summary>
+        /// <param name="e">Objeto estante</param>
+        /// <param name="p">Objeto producto</param>
+        /// <returns>true/false</returns>
+        public static bool operator ==(Estante e, Producto p)
+        {
+            if (e.productos.Contains(p))
+                return true;
+            else
+                return false;   
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+        public static bool operator +(Estante e, Producto p)
+        {
+            for(int i=0; i<e.productos.Length;i++)
+            {
+                if(e.productos[i] is null && e!=p) //!= comparo marca y codigo, no precio
+                {
+                    e.productos[i] = p;     //si hay lugar agrego el producto
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (e.productos[i] == p) //== comparo marca y codigo, no precio
+                {
+                    e.productos[i]=null;    //si es igual lo elimino 
+                    return e;
+                }
+            }
+            return e;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Ejercicios/CampusEj05_Clase05/Repaso/Negocio/Producto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Producto
+    {
+        #region Propiedades
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;       //como muestro siempre 2 decimales?
+        #endregion
+
+        #region Get
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        #endregion
+
+        #region Constructores
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+        #endregion
+
+        #region Metodos
+        public static string MostrarProducto(Producto p)
+        {
+            return String.Format("\nCodigo de barra: {0}\n" +
+                    "Marca: {1} \nPrecio: {2:#.00}\n", (string)p, p.GetMarca(), p.GetPrecio()); ;
+        }
+        #endregion
+
+        #region Sobrecarga
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            if (p.marca == marca)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+
+        #endregion
+
+
+
+    }
+}
